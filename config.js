bd5023970fbe7e11bff1060981820077fea5e3ca
Dat <datloi95@gmail.com>
2019-11-09 11:40:06 -0500

    facebook authorization

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '545807496208993',
+        clientSecret: 'f06ede9d1d1bd18b80c313ce5b48a19d'
+    }
 }
